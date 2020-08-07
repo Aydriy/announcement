230c4fdcf28c5611aa2697a30a241ddba1795a04
Aydriy <smitas4511@gmail.com>
2020-08-07 18:57:28 +0300

    Updates

---
+++ precache-manifest.6bb8b0f5ebbae27c4d365f9d70188634.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8262b4d9fcf5752cbd7326b1300eb3",
+    "url": "/announcement/index.html"
+  },
+  {
+    "revision": "250464cb44032e599fac",
+    "url": "/announcement/static/css/main.59dab58c.chunk.css"
+  },
+  {
+    "revision": "05c2c21cc57768c23e26",
+    "url": "/announcement/static/js/2.355ab99d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/announcement/static/js/2.355ab99d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250464cb44032e599fac",
+    "url": "/announcement/static/js/main.01d22d85.chunk.js"
+  },
+  {
+    "revision": "93cae4b2addee053f7bb",
+    "url": "/announcement/static/js/runtime-main.866f487b.js"
+  }
+]);
